5fd0b6a840c81e21525f07b86c33ca1f231b8bf4
Grayson Lang <grayson.lang@gmail.com>
2025-03-31 20:09:38 -0700

    simple

--- main.mjs
+++ main.mjs
@@ -1,76 +1,35 @@
-export const MATCH_PIXEL_RATIO = false;
-
-export function configureCanvasDensity(canvas) {
-  const context = canvas.getContext("2d", { colorSpace: "display-p3" });
-  const pixelRatio = window.devicePixelRatio ? window.devicePixelRatio : 1;
-  canvas.style.width = canvas.width + "px";
-  canvas.style.height = canvas.height + "px";
-  context.scale(pixelRatio, pixelRatio);
-  canvas.width = canvas.width * pixelRatio;
-  canvas.height = canvas.height * pixelRatio;
+import index_html from './index.html' assert { type: 'html' };
+// import image_a from './336699.png' assert { type: 'png' };
+// import image_b from './6699cc.png' assert { type: 'png' };
+export function getFilePaths() {
+  return {
+    index_html,
+    // image_a,
+    // image_b,
+  };
 }
 
-export function prepCanvas(src, canvas, scale = 1.0) {
-  canvas.width = src.width;
-  canvas.height = src.height;
-  if (MATCH_PIXEL_RATIO) {
-    const pixelRatio = window.devicePixelRatio ? window.devicePixelRatio : 1;
-    canvas.width = src.width / pixelRatio * scale;
-    canvas.height = src.height / pixelRatio * scale;
-    configureCanvasDensity(canvas);
-  } else {
-    canvas.width = src.width * scale;
-    canvas.height = src.height * scale;
-  }
-  const context = canvas.getContext("2d", { colorSpace: "display-p3" });
-  if (scale != 1.0) {
-    context.imageSmoothingEnabled = true;
-    context.imageSmoothingQuality = 'high';
-  }
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  return context;
-}
+const imageFiles = [
+  './336699.png',
+  './6699cc.png',
+];
 
-export function displayImage(image, canvas, scale = 1.0) {
-  prepCanvas(image, canvas, scale).drawImage(image, 0, 0, image.width * scale, image.height * scale);
-}
-
-
-export async function loadImage(src) {
+async function loadImage(src) {
   const image = new Image();
   image.src = src;
   await new Promise(x => image.onload = x);
   return image;
 }
 
-
-import index_html from './index.html' assert { type: 'html' }
-export function getFilePaths() {
-  return {
-    index_html,
-  };
-}
-
-import image_a from './336699.png' assert { type: 'png' };
-import image_b from './6699cc.png' assert { type: 'png' };
-const imageFiles = [
-  image_a,
-  image_b,
-];
-
-let currentIndex = 0;
-let srcImage;
-
 window.addEventListener("load", async () => {
-  const srcCanvas = document.body.appendChild(document.createElement('canvas'));
-  srcCanvas.width = srcCanvas.height = 0;
-  const delay_in_ms = 200;
+  let currentIndex = 0;
+  let srcImage;
+  const delay_in_ms = 500;
   async function timerFunc() {
-    currentIndex = (currentIndex + 1) % imageFiles.length;
     const path = imageFiles[currentIndex];
-    console.log(`Loading image - ${path}`);
+    console.log(`Loading image: ${path}`);
     srcImage = await loadImage(path);
-    // displayImage(srcImage, srcCanvas);
+    currentIndex = (currentIndex + 1) % imageFiles.length;
     setTimeout(timerFunc, delay_in_ms);
   }
   setTimeout(timerFunc, delay_in_ms);
--- serve.mjs
+++ serve.mjs
@@ -1,6 +1,5 @@
-import { parseArgs } from 'node:util';
+import path from 'node:path';
 import esbuild from 'esbuild';
-import { openOrReuseChromeTab } from './helpers.mjs';
 
 const noopPlugin = {
   name: 'noop-plugin',
@@ -14,78 +13,37 @@ const noopPlugin = {
       console.log('[onLoad]', args.path);
       return; // Let esbuild handle it
     });
-    // build.onEnd(async () => {
-    //   console.log(`✅ Build done`);
-    //   return;
-    // });
+
+    build.onEnd(args => {
+      console.log(`[onEnd] - ${Date.now()}`);
+    });
   },
 };
 
-async function main() {
-  const args = parseArgs({
-    allowNegative: true,
-    options: {
-      verbose: { type: 'boolean', short: 'v', default: false },
-      watch: { type: 'boolean', short: 'w', default: true },
-    },
+const userPort = 80;
+
+const options = {
+  assetNames: '[name]',
+  bundle: true,
+  entryPoints: ['./main.mjs'],
+  format: 'esm',
+  loader: {
+    '.html': 'file',
+    '.png': 'file',
+  },
+  outfile: 'dist/main.mjs',
+  outExtension: {'.js': '.mjs'},
+  plugins: [noopPlugin],
+};
+
+try {
+  const ctx = await esbuild.context(options);
+  await ctx.watch();
+  const { hosts, port } = await ctx.serve({
+    port: 80,
+    servedir: options.outdir || path.dirname(options.outfile),
   });
-
-  const verbose = args.values.verbose;
-
-  const userPort = 80;
-
-  const bannerScript = `(() => {
-    const s = new EventSource('/esbuild');
-    s.addEventListener('change', () => location.reload());
-    s.addEventListener('error', () => s.close());
-    })();`;
-
-  const options = {
-    assetNames: '[name]', //'assets/[name]-[hash]',
-    banner: { js: bannerScript },
-    bundle: true,
-    entryPoints: ['./main.mjs'],
-    format: 'esm',
-    loader: {
-      '.css': 'file',
-      '.html': 'file',
-      '.png': 'file',
-    },
-    outdir: 'dist',
-    outExtension: {'.js': '.mjs'},
-    plugins: [
-      noopPlugin,
-      // {
-      //   name: 'build-complete',
-      //   setup(build) {
-      //     build.onEnd(async () => {
-      //       console.log(`✅ Build done`);
-      //     });
-      //   },
-      // },
-    ],
-  };
-
-  try {
-    const ctx = await esbuild.context(options);
-    if (args.values.watch) {
-      await ctx.watch();
-      console.log('Watching for changes...');
-    }
-
-    const { hosts, port } = await ctx.serve({
-      port: userPort,
-      servedir: options.outdir || path.dirname(options.outfile),
-    });
-
-    const portString = (userPort == 80 ? '' : (':' + userPort));
-    openOrReuseChromeTab(`http://localhost${portString}`, { verbose });
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
-}
-
-if (import.meta.url === `file://${process.argv[1]}`) {
-  main();
+} catch (err) {
+  console.error(err);
+  process.exit(1);
 }
